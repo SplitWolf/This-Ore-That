23dda37c5c1a621a49651727b2daeec2dff94e0f
SplitWolf <mat.jake1360@gmail.com>
2024-06-23 20:38:32 -0400

    refactor: rename Config to ThisOreThat Config & Change Parameters

---
+++ src/main/java/dev/splitwolf/thisorethat/ThisOreThatConfig.java
@@ -0,0 +1,24 @@
+package dev.splitwolf.thisorethat;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@Mod.EventBusSubscriber(modid = ThisOreThat.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ThisOreThatConfig
+{
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    public static final ForgeConfigSpec.BooleanValue enableSmeltingRecipes = BUILDER
+            .comment("Whether to add the smelting recipes for the ores or not")
+            .define("enableSmeltingRecipes", true);
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+    }
+}
