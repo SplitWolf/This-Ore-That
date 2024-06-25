e511a349332872236eeb28809c6676865a77b33e
SplitWolf <mat.jake1360@gmail.com>
2024-06-25 01:32:13 -0400

    fix: Fixed swapped ore block sounds.

--- src/main/java/dev/splitwolf/thisorethat/block/OreBlocks.java
+++ src/main/java/dev/splitwolf/thisorethat/block/OreBlocks.java
@@ -5,9 +5,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -53,9 +51,9 @@ public class OreBlocks {
 
     private static RegistryObject<Block> addOre(String name) {
         if(name.contains("deepslate")) {
-            return registerBlock(name,() -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+            return registerBlock(name,() -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE)));
         }
-        return registerBlock(name,() -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE)));
+        return registerBlock(name,() -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
     }
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
